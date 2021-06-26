15cf49df23aea5360246f3708b17f161a606f679
Mark Ustyugov <mark.ustyugov@gmail.com>
2021-06-26 18:32:16 +0500

    адаптив, меню без js, плавный скролл меню

--- source/js/script.js
+++ source/js/script.js
@@ -14,19 +14,83 @@
 //   }
 // });
 
+var pageHeader = document.querySelector('.page__header--nojs');
+
 var map = document.querySelector('.contacts__map-wrapper');
 var menuButton = document.querySelector('.header__menu-button');
 var menu = document.querySelector('.navigation');
 var body = document.querySelector('.page');
 
-menuButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  menuButton.classList.toggle('header__menu-button--closed');
-  menu.classList.toggle('navigation--active');
-  body.classList.toggle('scroll-lock');
-});
+var phone = document.querySelector('input[type=tel]');
+var form = document.querySelector('.form-order');
 
-map.classList.add('contacts__map-wrapper--hide');
+var smoothLinks = document.querySelectorAll('a[href^="#link"]');
+
+if (menuButton) {
+  menuButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    menuButton.classList.toggle('header__menu-button--closed');
+    menu.classList.toggle('navigation--active');
+    body.classList.toggle('scroll-lock');
+  });
+}
+
+if (pageHeader) {
+  pageHeader.classList.remove('page__header--nojs');
+}
+
+if (map) {
+  map.classList.add('contacts__map-wrapper--hide');
+}
+
+if (smoothLinks) {
+  smoothLinks.forEach(function (item) {
+    item.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      menuButton.classList.add('header__menu-button--closed');
+      menu.classList.remove('navigation--active');
+      body.classList.remove('scroll-lock');
+
+      var id = item.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+}
+
+if (form) {
+  form.addEventListener('submit', formSend);
+}
+
+function formSend(evt) {
+  evt.preventDefault();
+
+  var error = formValidate(form);
+
+  if (error > 0) {
+    document.querySelector('.form-order__field--phone').classList.add('form-order__field--error');
+  } else {
+    document.querySelector('.form-order__field--phone').classList.remove('form-order__field--error');
+  }
+}
+
+function formValidate() {
+  var error = 0;
+
+  if (phoneTest(phone)) {
+    error++;
+  }
+
+  return error;
+}
+
+function phoneTest(input) {
+  return !/[1-9]/.test(input.value);
+}
 
 if (document.querySelector('.contacts__map-wrapper')) {
   ymaps.ready(function () {
@@ -59,34 +123,4 @@ if (document.querySelector('.contacts__map-wrapper')) {
     myMap.geoObjects
       .add(myPlacemark)
   });
-}
-var phone = document.querySelector('input[type=tel]');
-var form = document.querySelector('.form-order');
-
-form.addEventListener('submit', formSend);
-
-function formSend(evt) {
-  evt.preventDefault();
-
-  var error = formValidate(form);
-
-  if (error > 0) {
-    document.querySelector('.form-order__field--phone').classList.add('form-order__field--error');
-  } else {
-    document.querySelector('.form-order__field--phone').classList.remove('form-order__field--error');
-  }
-}
-
-function formValidate() {
-  var error = 0;
-
-  if (phoneTest(phone)) {
-    error++;
-  }
-
-  return error;
-}
-
-function phoneTest(input) {
-  return !/[1-9]/.test(input.value);
 }

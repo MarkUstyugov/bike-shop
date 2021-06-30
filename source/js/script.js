d6427d64a53674d4b97a625f0aef289410c3339d
Mark Ustyugov <mark.ustyugov@gmail.com>
2021-06-30 19:14:19 +0500

    исправленны замечания

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,4 @@
 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
 
 var pageHeader = document.querySelector('.page__header--nojs');
 
@@ -21,7 +7,6 @@ var menuButton = document.querySelector('.header__menu-button');
 var menu = document.querySelector('.navigation');
 var body = document.querySelector('.page');
 
-// var nameInput = document.querySelector('input[type=text]');
 var phone = document.querySelector('input[type=tel]');
 var form = document.querySelector('.form-order');
 
@@ -39,10 +24,6 @@ if (menuButton) {
     body.classList.toggle('scroll-lock');
   });
 }
-
-// if (map) {
-//   map.classList.add('contacts__map-wrapper--hide');
-// }
 
 if (smoothLinks) {
   smoothLinks.forEach(function (item) {
@@ -93,36 +74,3 @@ function formValidate() {
 function phoneTest(input) {
   return !/^\d+$/.test(input.value);
 }
-
-// if (document.querySelector('.contacts__map-wrapper')) {
-//   ymaps.ready(function () {
-//     var myMap = new ymaps.Map('map', {
-//       center: [59.938536, 30.323324],
-//       zoom: 14
-//     }),
-
-//       // Создаём макет содержимого.
-//       // MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-//       //   '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-//       // ),
-
-//       myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {
-//         hintContent: "Магазин велосипедов",
-//         balloonContent: "г. Санкт-Петербург, ул. Большая Конюшенная"
-//       }, {
-//         // Опции.
-//         // Необходимо указать данный тип макета.
-//         iconLayout: "default#image",
-//         // Своё изображение иконки метки.
-//         // iconImageHref: "img/map-pin.svg",
-//         // Размеры метки.
-//         // iconImageSize: [67, 100],
-//         // Смещение левого верхнего угла иконки относительно
-//         // её "ножки" (точки привязки).
-//         // iconImageOffset: [-30, -100]
-//       });
-
-//     myMap.geoObjects
-//       .add(myPlacemark)
-//   });
-// }
